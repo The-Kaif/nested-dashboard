7e74e44c0d7eb89a7d02458bdd4b818d2f19ee9e
kaif-epicglobal <kaifmohammad5990@gmail.com>
2024-06-16 15:34:07 +0530

    remove build error of image

---
+++ next.config.js
@@ -0,0 +1,16 @@
+// next.config.js
+
+module.exports = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "http",
+        hostname: "**",
+      },
+      {
+        protocol: "https",
+        hostname: "**",
+      },
+    ],
+  },
+};
